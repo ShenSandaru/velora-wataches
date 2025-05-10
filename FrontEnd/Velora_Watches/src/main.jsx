80976fca91f8f06b0be045ee75ac3c7794763eaa
Shen Sandaru <136662438+ShenSandaru@users.noreply.github.com>
2025-05-10 19:29:12 +0530

    fix some errors

--- FrontEnd/Velora_Watches/src/main.jsx
+++ FrontEnd/Velora_Watches/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './styles/global.css' // Import global styles for responsive design
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
